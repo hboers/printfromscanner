c44f00f4ed6edfb1b2d361520a3b5258d18e57f0
Robert Richardson <robrich22@gmail.com>
2014-12-28 20:32:56 -0500

    Initial Commit

---
+++ PrintFromScanner/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PrintFromScanner
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEvents = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPrintAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(485, 13);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(71, 21);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Folder To Monitor";
+            // 
+            // txtEvents
+            // 
+            this.txtEvents.Location = new System.Drawing.Point(15, 50);
+            this.txtEvents.Multiline = true;
+            this.txtEvents.Name = "txtEvents";
+            this.txtEvents.ReadOnly = true;
+            this.txtEvents.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEvents.Size = new System.Drawing.Size(541, 195);
+            this.txtEvents.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(425, 256);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(131, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::PrintFromScanner.Properties.Settings.Default, "ScanFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtFolder.Location = new System.Drawing.Point(108, 14);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(374, 20);
+            this.txtFolder.TabIndex = 1;
+            this.txtFolder.Text = global::PrintFromScanner.Properties.Settings.Default.ScanFolder;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 255);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(140, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start Monitor";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPrintAll
+            // 
+            this.btnPrintAll.Location = new System.Drawing.Point(161, 256);
+            this.btnPrintAll.Name = "btnPrintAll";
+            this.btnPrintAll.Size = new System.Drawing.Size(140, 23);
+            this.btnPrintAll.TabIndex = 6;
+            this.btnPrintAll.Text = "Print All In Folder";
+            this.btnPrintAll.UseVisualStyleBackColor = true;
+            this.btnPrintAll.Click += new System.EventHandler(this.btnPrintAll_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 291);
+            this.Controls.Add(this.btnPrintAll);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtEvents);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "frmMain";
+            this.Text = "Print from Scanner Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEvents;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPrintAll;
+    }
+}
+
---
+++ PrintFromScanner/frmMain.cs
@@ -0,0 +1,145 @@
+﻿
+/*  Program Name   :  PrintFromScanner
+ *  Author         :  Robert Richardson
+ *  Date           :  Decemeber 2014
+ *  Description    :  Monitors folder for new files.  When a new .pdf appears in the folder, the program will send the 
+ *                    pdf to the default printer. 
+*/
+
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace PrintFromScanner
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeWatcher()
+        {
+            WriteEvent(String.Format("Monitoring Folder {0}.", Properties.Settings.Default.ScanFolder));
+            FileSystemWatcher fsWatch = new FileSystemWatcher(Properties.Settings.Default.ScanFolder, "*.pdf");
+            fsWatch.EnableRaisingEvents = true;
+            fsWatch.Created += fsWatch_Created; 
+        }
+
+        private void SendToPrinter(string FilePath)
+        {
+            try
+            {
+                WriteEvent(String.Format("Sending File To Printer {0}.",FilePath));
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.Verb = "print";
+                info.FileName = FilePath;
+                info.CreateNoWindow = true;
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();               
+            }
+            catch(Exception ex)
+            {
+                WriteEvent(ex.ToString());
+            }
+        }       
+
+        private void fsWatch_Created(object sender, FileSystemEventArgs e)
+        {          
+            SendToPrinter(e.FullPath);
+        }        
+
+        private void WriteEvent(string msg)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(txtEvents.AppendText), new object[] { msg });
+                return;
+            }
+                    
+            txtEvents.AppendText(String.Format("{0}: {1}\n", DateTime.Now.ToShortTimeString(), msg));           
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            DialogResult result;
+            
+            result = folder.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                Properties.Settings.Default.ScanFolder = folder.SelectedPath;
+                Properties.Settings.Default.Save();
+                txtFolder.Refresh();                              
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+
+            result = MessageBox.Show("Are you sure you want to exit?", "Exit Program", MessageBoxButtons.YesNo);
+
+            if(result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+       
+        private void btnStart_Click(object sender, EventArgs e)
+        {            
+            if (Directory.Exists(Properties.Settings.Default.ScanFolder))
+            {
+                DialogResult result;
+                result = MessageBox.Show("Are you sure you want to start active monitoring?  Any PDF files created in the monitored folder will be sent to the printer.", 
+                                         "Start Monitor",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    InitializeWatcher();
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must select a folder to monitor!", "Select Folder!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnPrintAll_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+
+            result = MessageBox.Show("Are you sure you want to print all the .PDF files in the selected folder?",
+                                     "Print All Files", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (Directory.Exists(Properties.Settings.Default.ScanFolder))
+                {
+                    string[] Files = Directory.GetFiles(Properties.Settings.Default.ScanFolder, "*.pdf");
+
+                    if (Files.Length > 0)
+                    {
+                        WriteEvent("Printing All Files.");
+
+                        foreach (string s in Files)
+                        {
+                            SendToPrinter(s);
+                        }
+                    }
+                    else
+                    {
+                        WriteEvent("No .PDF files were found in the folder.");
+                    }
+                }
+            }
+
+        }
+    }
+}
